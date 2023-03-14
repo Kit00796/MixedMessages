3eda12db338165f716ff9066aeb7e34d122199e0
Kit00796 <116655316+Kit00796@users.noreply.github.com>
2023-03-14 15:08:57 -0400

    finished sans bugs

--- MixedMessages/main.js
+++ MixedMessages/main.js
@@ -1,11 +1,47 @@
 const dinner = {
+
     style: ['Spanish', 'Chinese', 'Italian', 'Thai', 'Indian', 'Mediterranean' ],
     protein: ['chicken', 'beef', 'pork', 'tofu', 'fish', 'egg'],
     veg: ['broccoli', 'onion', 'zucchini', 'tomato', 'corn', 'potato'],
     body: ['rice', 'pasta', 'greens', 'bread', 'broth']
+
 };
 
 const randomNum = num => {
+
     return Math.floor(Math.random() * num);
+
 };
 
+const buildDinner = () => {
+    
+    let dinnerIdea = [];
+
+    for (let arr in dinner){
+
+        let mixUp = randomNum(dinner[arr].length)
+
+        switch (arr){
+            case 'style':
+                dinnerIdea.push(`Tonight for dinner, you should have ${dinner[arr][mixUp]}`);
+                break;
+            case 'protein':
+                dinnerIdea.push(` ${dinner[arr][mixUp]}`);
+                break;
+            case 'veg':
+                dinnerIdea.push(` and ${dinner[arr][mixUp]}`);
+                break;
+            case 'body':
+                dinnerIdea.push(` over ${dinner[arr][mixUp]}`);
+                break;
+            default: 
+                'NOTHING';
+                break;
+        }
+    };
+
+    return dinnerIdea.join();
+
+};
+
+console.log(buildDinner());
