59a8a4047fca0c2e47b5359d29b703faafb5932e
Kit00796 <116655316+Kit00796@users.noreply.github.com>
2023-03-14 14:18:48 -0400

    main topic down

--- MixedMessages/main.js
+++ MixedMessages/main.js
@@ -1,1 +1,11 @@
-//start
+const dinner = {
+    style: ['Spanish', 'Chinese', 'Italian', 'Thai', 'Indian', 'Mediterranean' ],
+    protein: ['chicken', 'beef', 'pork', 'tofu', 'fish', 'egg'],
+    veg: ['broccoli', 'onion', 'zucchini', 'tomato', 'corn', 'potato'],
+    body: ['rice', 'pasta', 'greens', 'bread', 'broth']
+};
+
+const randomNum = num => {
+    return Math.floor(Math.random() * num);
+};
+
